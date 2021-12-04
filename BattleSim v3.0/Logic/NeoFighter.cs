c8b855380025becd5004d24a53a8861a0bba9242
KaanGogcay <Kaangogcay@gmail.com>
2021-12-04 12:07:14 +0100

    Started over, Code is so clean. Used in inheritance, beautiful

---
+++ BattleSim v3.0/Logic/NeoFighter.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    public enum NeoFighterNames
+    {
+        Korbat,
+        Grarrl,
+        Blumaroo
+    }
+
+    public class NeoFighter
+    {
+        public Random rnd = new Random();
+
+        public NeoFighterNames Name { get; private set; }
+        public int Health { get; private set; }
+        public int AttackPower { get; private set; }
+        public int CritRatio { get; private set; }
+
+        internal void SetName(NeoFighterNames name)
+        {
+            Name = name;
+        }
+        internal void SetHealth(int health)
+        {
+            Health = health;
+        }
+        internal void SetAttackPower(int attackPower)
+        {
+            AttackPower = attackPower;
+        }
+        /// <summary>Chance in percentage. Example 30 means 30%</summary>
+        internal void SetCritRatio(int critRatio)
+        {
+            CritRatio = critRatio;
+        }
+
+        public void LoseHealth(int damage)
+        {
+            Health = Health - damage;
+        }
+        public int Attack(Random rnd, int attackpower)
+        {
+            int attackModifier = rnd.Next(-2, 3);
+            return attackpower + attackModifier;
+        }
+    }
+}
+
---
+++ BattleSim v3.0/Logic/NeoFighters/Korbat.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic.NeoFighters
+{
+    public class Korbat : NeoFighter
+    {
+        public Korbat()
+        {
+            SetName(NeoFighterNames.Korbat);
+            SetHealth(280);
+            SetAttackPower(28);
+            SetCritRatio(15);
+        }
+
+        public int Attack1_LifeSteal()
+        {
+            int attack = Attack(rnd, AttackPower);
+            Heal(attack);
+            return attack;
+        }
+        public void Attack2_Strychine()
+        {
+            //double damage if enemy is poisoned
+        }
+        public void Attack3_PoisonBite()
+        {
+            //30% chance to poison
+        }
+
+
+
+        void Heal(int stolenHealth)
+        {
+            SetHealth(Health + stolenHealth);
+        }
+    }
+}
